554736af39e8734cf12774383e5e833f30d35b57
Arthur <arturiopr1994@gmail.com>
2022-07-13 16:50:57 +0300

    18 Use memo

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,46 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: "useMemo"
+}
+
+export const Exp = () => {
+
+    const [a, setA] = useState(5)
+    const [b, setB] = useState(5)
+
+    let resultA = 1
+    let resultB = 1
+
+    resultA = useMemo(() => {
+        let tempResult = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 10) {
+                fake++;
+                const fakeV = Math.random()
+            }
+            tempResult = tempResult * i;
+        }
+        return tempResult
+    }, [a])
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i;
+    }
+
+    return (
+        <div>
+            <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
+            <input value={b} onChange={(e) => setB(Number(e.currentTarget.value))}/>
+            <hr/>
+            <div>
+                Result for a: {resultA}
+            </div>
+            <div>
+                Result for b: {resultB}
+            </div>
+        </div>)
+}
+
